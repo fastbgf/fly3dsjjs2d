ffa259f906f320c7b92110b36b0114f7876458b4
fastbgf <133555992+fastbgf@users.noreply.github.com>
2023-06-26 08:34:11 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'c664751b-b662-48f9-b2aa-2f0d4d337a7f'}
+UUID=${UUID:-'eeef5d69-4190-4364-b19c-4301ba6451ca'}
 
 # 其他Paas保活
-PAAS1_URL=
+PAAS1_URL=jd8sdhfjsh2khdh.fly.dev
 PAAS2_URL=
 PAAS3_URL=
 PAAS4_URL=
@@ -17,8 +17,8 @@ KOYEB_ACCOUNT=
 KOYEB_PASSWORD=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH=''
-ARGO_DOMAIN=
+ARGO_AUTH='{"AccountTag":"0bbaad936c5ec10b10132c290caffe95","TunnelSecret":"vn1Z76MHwgNrmAeKFzvJMhUtUAc2x2NmWHDI2ErPsVk=","TunnelID":"dc9f8921-be3e-435b-94f5-931fa155400c"}'
+ARGO_DOMAIN=flythe3yg.hellostar.cf
 
 generate_config() {
   cat > config.json << EOF
